f0621995960f4537f4a30d30fdc201dea2268d87
RockCWRU <73302942+RockCWRU@users.noreply.github.com>
2021-03-17 09:50:38 -0400

    Horizon Task Code

---
+++ AAA_Horizon.m
@@ -0,0 +1,5 @@
+cd % this is hopefully all you need to change
+istest = 0;
+mainpath = 'C:\Program Files\Matlab\DATA_HORIZONTASK\HorizonTask\horizon_task\'; 
+datadir = 'C:\Program Files\Matlab\DATA_HORIZONTASK\HorizonTask\horizon_task\DATA_HORIZONTASK\'; 
+main_horizon;
